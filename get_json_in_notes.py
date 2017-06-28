48fe518a12429a24f8f2923d536d4b26d4b28ac5
Edmund Dipple <edmund.dipple@devopsguys.com>
2017-06-28 14:42:17 +0100

    Insert images rather than replacing placeholders

--- get_json_in_notes.py
+++ get_json_in_notes.py
@@ -1,10 +1,11 @@
 import sys
 import json
 from pptx import Presentation
+from pptx.util import Inches, Pt
 
 def delete_slide(prs, slide):
     #Make dictionary with necessary information
-    id_dict = { slide.id: [i, slide.rId] for i,slide in enumerate(prs.slides._sldIdLst) }
+    id_dict = {slide.id: [i, slide.rId] for i, slide in enumerate(prs.slides._sldIdLst)}
     slide_id = slide.slide_id
     prs.part.drop_rel(id_dict[slide_id][1])
     del prs.slides._sldIdLst[id_dict[slide_id][0]]
@@ -12,17 +13,23 @@ def delete_slide(prs, slide):
 print 'Number of arguments:', len(sys.argv), 'arguments.'
 print 'Argument List:', str(sys.argv)
 
-prs = Presentation(sys.argv[1])
+PRS = Presentation(sys.argv[1])
 
-for slide in prs.slides:
-    if(slide.has_notes_slide):
+width = height = Inches(1)
+left = top = Inches(2)
+
+for slide in PRS.slides:
+    g = "/Users/edmundd/Desktop/logo.gif"
+    top = PRS.slide_height - Inches(1)
+    pic = slide.shapes.add_picture(g, left, top, width, height)
+
+    if slide.has_notes_slide:
         text_frame = slide.notes_slide.notes_text_frame
-        print text_frame.text
         try:
             metadata = json.loads(text_frame.text)
-            if("hello" in metadata['tags']):
-                delete_slide(prs,slide)
+            if "hello" in metadata['tags']:
+                delete_slide(PRS, slide)
         except:
             pass
 
-prs.save('/Users/edmundd/Desktop/Test Presentation2.pptx')
+PRS.save('/Users/edmundd/Desktop/Test Presentation2.pptx')
