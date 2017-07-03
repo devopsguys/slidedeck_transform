742f49232cc337e87af4b0baaf35ab63045f6ac8
Edmund Dipple <edmund.dipple@devopsguys.com>
2017-07-03 14:37:53 +0100

    Defensively check if json string is valid json

--- tag_recognition.py
+++ tag_recognition.py
@@ -7,19 +7,31 @@ import regex
 
 TEMP_LOGO_FILE = "temp_logo.png"
 
+
+def is_json(text):
+    try:
+        json.loads(text)
+    except ValueError:
+        return False
+    return True
+
+
 def get_tags_in_comments(text):
     json_list = find_json_in_string(text)
     all_tags = []
     if json_list:
         for json_block in json_list:
-            tags = json.loads(json_block)['tags']
-            if not isinstance(tags, list):
-                tags = [tags]
-            all_tags += tags
+            if is_json(json_block):
+                tags = json.loads(json_block)['tags']
+                if not isinstance(tags, list):
+                    tags = [tags]
+                all_tags += tags
     return all_tags
+
 
 def find_json_in_string(text):
     return regex.findall('{(?:[^{}]|(?R))*}', text)
+
 
 def trim(image):
     background = Image.new(image.mode, image.size, image.getpixel((0, 0)))
@@ -29,12 +41,15 @@ def trim(image):
     if bbox:
         return image.crop(bbox)
 
+
 def delete_slide(prs, slide):
-    #Make dictionary with necessary information
-    id_dict = {slide.id: [i, slide.rId] for i, slide in enumerate(prs.slides._sldIdLst)}
+    # Make dictionary with necessary information
+    id_dict = {slide.id: [i, slide.rId]
+               for i, slide in enumerate(prs.slides._sldIdLst)}
     slide_id = slide.slide_id
     prs.part.drop_rel(id_dict[slide_id][1])
     del prs.slides._sldIdLst[id_dict[slide_id][0]]
+
 
 def main():
 
@@ -63,13 +78,14 @@ def main():
 
         if slide.has_notes_slide:
             text_frame = slide.notes_slide.notes_text_frame
-            try: #TODO remove this try-except and replace with more defensive methods
-                metadata = json.loads(text_frame.text)
-                if "hello" in metadata['tags']:
-                    delete_slide(presentation, slide)
-            except:
-                pass
+            tags = get_tags_in_comments(text_frame.text)
+            if "hello" in tags:
+                delete_slide(presentation, slide)
 
         os.remove(TEMP_LOGO_FILE)
 
     presentation.save('/Users/edmundd/Desktop/Test Presentation2.pptx')
+
+
+if __name__ == '__main__':
+    main()
--- tag_recognition_test.py
+++ tag_recognition_test.py
@@ -1,27 +1,51 @@
 import unittest
 import slidedeck_transform.tag_recognition
 
+
 class TestTagRecognition(unittest.TestCase):
 
-    def test_no_tags_in_comments(self):
+    def test_is_json_detection(self):
+        self.assertTrue(slidedeck_transform.tag_recognition.is_json("{}"))
+        self.assertFalse(slidedeck_transform.tag_recognition.is_json("{!}"))
+
+    def test_no_tags(self):
         tags = slidedeck_transform.tag_recognition.get_tags_in_comments("")
         self.assertEqual(tags, [])
 
-    def test_one_tag_in_comments(self):
-        tags = slidedeck_transform.tag_recognition.get_tags_in_comments('{"tags": "tag1"}')
+    def test_one_tag(self):
+        tags = slidedeck_transform.tag_recognition.get_tags_in_comments(
+            'ab{"tags": "tag1"}cd'
+        )
         self.assertEqual(tags, ['tag1'])
 
-    def test_list_of_tags_in_comments(self):
-        tags = slidedeck_transform.tag_recognition.get_tags_in_comments('{"tags": ["tag1","tag2"]}')
+    def test_list_of_tags(self):
+        tags = slidedeck_transform.tag_recognition.get_tags_in_comments(
+            'ab{"tags": ["tag1","tag2"]}cd'
+        )
         self.assertEqual(tags, ['tag1', 'tag2'])
-    
-    def test_multiple_lists_of_tags_in_comments(self):
-        tags = slidedeck_transform.tag_recognition.get_tags_in_comments('{"tags": ["tag1","tag2"]}blah{"tags": ["tag3","tag4"]}')
-        self.assertEqual(tags, ['tag1', 'tag2','tag3','tag4'])
 
-    def test_a_list_and_a_single_tag_in_comments(self):
-        tags = slidedeck_transform.tag_recognition.get_tags_in_comments('{"tags": ["tag1","tag2"]}blah{"tags": "tag3"}')
-        self.assertEqual(tags, ['tag1', 'tag2','tag3'])
+    def test_multiple_lists_of_tags(self):
+        tags = slidedeck_transform.tag_recognition.get_tags_in_comments(
+            'ab{"tags": ["tag1","tag2"]}blah{"tags": ["tag3","tag4"]}cd'
+        )
+        self.assertEqual(tags, ['tag1', 'tag2', 'tag3', 'tag4'])
+
+    def test_a_list_and_a_single_tag(self):
+        tags = slidedeck_transform.tag_recognition.get_tags_in_comments(
+            'ab{"tags": ["tag1","tag2"]}blah{"tags": "tag3"}cd'
+        )
+        self.assertEqual(tags, ['tag1', 'tag2', 'tag3'])
+
+    def test_invalid_json(self):
+        tags = slidedeck_transform.tag_recognition.get_tags_in_comments(
+            'ab{"tags": ["tag1","tag2"}cd'
+        )
+        self.assertEqual(tags, [])
+
+    def test_valid_json_but_no_tags(self):
+        tags = slidedeck_transform.tag_recognition.get_tags_in_comments(
+            'ab{"bags": ["tag1","tag2"}cd')
+        self.assertEqual(tags, [])
 
 
 if __name__ == '__main__':
