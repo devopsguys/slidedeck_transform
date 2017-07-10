a7f5ee68f159d67b44a1816f71f124a4c081fb45
Edmund Dipple <edmund.dipple@devopsguys.com>
2017-07-10 14:18:41 +0100

    Check for missing dependencies in Unix environments

--- run.sh
+++ run.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+command -v npm >/dev/null 2>&1 || { echo "NodeJS is not installed. It is required in order to run this application." >&2; exit 1; }
+command -v python >/dev/null 2>&1 || { echo "Python is not installed. It is required in order to run this application." >&2; exit 1; }
+
 pip install -r requirements.txt
 pushd electron-gui && npm install && npm start
 popd
