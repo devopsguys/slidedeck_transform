8bbf61d578f4f8f6d7fc6aa2d7e551998261c8be
Edmund Dipple <edmund.dipple@devopsguys.com>
2017-07-11 09:24:00 +0100

    Strip whitespace from template placeholders, and the templates list should not contain duplicates

--- sdt_string_replace.py
+++ sdt_string_replace.py
@@ -1,11 +1,12 @@
 import regex
+import sdt_common
 
 
 def find_templates_in_string(text):
     templates = regex.findall('{{.*?}}', text)
 
     def strip_brackets(x):
-        return x[2:-2]
+        return x[2:-2].strip()
 
     return list(map(strip_brackets, templates))
 
@@ -20,7 +21,7 @@ def find_all_template_strings_in_presentation(presentation):
             for paragraph in text_frame.paragraphs:
                 for template in find_templates_in_string(paragraph.text):
                     templates += [template]
-    return templates
+    return sdt_common.uniq(templates)
 
 
 def replace_template_string(slide, template, text):
@@ -29,6 +30,6 @@ def replace_template_string(slide, template, text):
             continue
         text_frame = shape.text_frame
         for paragraph in text_frame.paragraphs:
-            if "{{{0}}}".format(template) in paragraph.text: 
+            if "{{{0}}}".format(template) in paragraph.text:
                 paragraph.text = paragraph.text.replace(
                     '{{' + template + '}}', text)
