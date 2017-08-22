41bf0b7ed732f2eedd20ef9283891fceffc64994
Edmund Dipple <edmund.dipple@devopsguys.com>
2017-08-22 16:40:50 +0100

    Return templates as a list of name:default value pairs

--- slidedeck_transform/sdt_string_replace.py
+++ slidedeck_transform/sdt_string_replace.py
@@ -1,5 +1,6 @@
 import regex
 import sdt_common
+import json
 
 
 def strip_brackets(x):
@@ -13,7 +14,10 @@ def find_templates_in_string(text):
 
 
 def find_all_template_strings_in_presentation(presentation):
+
+    template_names = []
     templates = []
+
     for slide in presentation.slides:
         for shape in slide.shapes:
             if not shape.has_text_frame:
@@ -21,8 +25,26 @@ def find_all_template_strings_in_presentation(presentation):
             text_frame = shape.text_frame
             for paragraph in text_frame.paragraphs:
                 for template in find_templates_in_string(paragraph.text):
-                    templates += [template]
-    return sdt_common.uniq(templates)
+                    template_names += [template]
+    for template in sdt_common.uniq(template_names):
+        templates.append(
+            {"name": template, "default": get_template_default(template, presentation)})
+    return templates
+
+
+def get_template_default(name, presentation):
+    first_slide = presentation.slides[0]
+
+    if first_slide.has_notes_slide:
+        text = first_slide.notes_slide.notes_text_frame.text
+        json_list = sdt_common.find_json_in_string(text)
+        if json_list:
+            for json_block in json_list:
+                if sdt_common.is_json(json_block):
+                    json_data = json.loads(json_block)
+                    if json_data.get('defaults', {}).get(name):
+                        return json_data['defaults'][name]
+    return ""
 
 
 def replace_template_string(slide, template, text):
